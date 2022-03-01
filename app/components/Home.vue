1a2f69d5ca952f327328527e3b544b6c9ee87a73
Selecta <odilzhon100@gmail.com>
2022-03-01 15:56:16 +0500

    Init project

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,41 +1,36 @@
 <template>
-    <Page>
-        <ActionBar>
-            <Label text="Home"/>
-        </ActionBar>
+  <Page>
+    <ActionBar>
+      <Label text="Home" />
+    </ActionBar>
 
-        <GridLayout>
-            <Label class="info">
-                <FormattedString>
-                    <Span class="fas" text.decode="&#xf135; "/>
-                    <Span :text="message"/>
-                </FormattedString>
-            </Label>
-        </GridLayout>
-    </Page>
+    <AbsoluteLayout>
+      <ListView >
+        <ListView>
+          <Label text="label text"></Label>
+        </ListView>
+      </ListView>
+
+      <GridLayout id="btnFab" @tap="onButtonTap"> </GridLayout>
+    </AbsoluteLayout>
+  </Page>
 </template>
 
 <script>
-  export default {
-    computed: {
-      message() {
-        return "Blank {N}-Vue app";
-      }
-    }
-  };
+export default {
+  computed: {
+    message() {
+      return "Blank {N}-Vue app";
+    },
+  },
+};
 </script>
 
 <style scoped lang="scss">
-    @import '@nativescript/theme/scss/variables/blue';
-
-    // Custom styles
-    .fas {
-        @include colorize($color: accent);
-    }
-
-    .info {
-        font-size: 20;
-        horizontal-align: center;
-        vertical-align: center;
-    }
+#btnFAB {
+  width: 56;
+  height: 56;
+  background-color: orangered;
+  border-radius: 28;
+}
 </style>
